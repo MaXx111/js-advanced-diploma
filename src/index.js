e31fbabc089e4e5772f3ea10234a3a9c0fdb1fef
Sidorov Maksim <maxx11167@gmail.com>
2023-02-13 16:40:09 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import './css/style.css';
 import './js/app.js';
 
-import './js/generators.js';
-import './js/Team.js';
-import './js/GamePlay.js';
-import './js/GameController.js';
-
 // Точка входа webpack
 // Не пишите код в данном файле
